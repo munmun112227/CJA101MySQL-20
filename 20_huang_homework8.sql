b7f5b178dbbb2429abc1ee4b40cb2c0489cf7238
Jason Huang <virage1257@hotmail.com>
2025-03-19 15:59:03 +0800

    homework8 is done

---
+++ 20_huang_homework8.sql
@@ -0,0 +1,71 @@
+-- 1/ 新建DEPARTMENT資料表
+
+CREATE TABLE DEPARTMENT
+( id INT NOT NULL,
+  name VARCHAR(24) NOT NULL
+);
+DESC EMPLOYEE;
+
+-- 2/ 利用DEPT資料表中的資料，將資料新增至DEPARTMENT資料表中(只新增相對的資料欄)。
+
+INSERT INTO DEPARTMENT
+	SELECT deptno, dname
+    FROM dept;
+
+-- 3/ 新建EMPLOYEE資料表。
+
+CREATE TABLE EMPLOYEE
+( id INT NOT NULL,
+  last_name VARCHAR(24) NOT NULL,
+   first_name VARCHAR(24),
+   dept_id INT
+);
+DESC EMPLOYEE;
+
+-- 4/ 將EMPLOYEE資料表中last_name欄位的資料型態更改為varchar(40)。
+
+ALTER TABLE employee
+MODIFY COLUMN last_name varchar(40);
+DESC EMPLOYEE;
+
+-- 5/ 使用EMP資料表的結構中之EMPNO, ENAME及DEPTNO之定義來新建EMPLOYEE2資料表，並
+-- 將欄位名稱設定為ID, LAST_NAME及DEPT_ID。
+
+CREATE TABLE EMPLOYEE2
+AS
+SELECT empno AS id, ename  AS LAST_NAME, deptno AS dept_id
+	FROM emp;
+DESC EMPLOYEE2;
+
+-- 6/ 刪除整個EMPLOYEE資料表。
+
+DROP TABLE EMPLOYEE;
+
+-- 7/ 將EMPLOYEE2資料表改名為EMPLOYEE。
+
+ALTER TABLE EMPLOYEE2
+RENAME TO EMPLOYEE;
+DESC EMPLOYEE;
+-- 8/ 將EMPLOYEE資料表中的LAST_NAME欄位刪除。
+
+ALTER TABLE EMPLOYEE
+DROP COLUMN LAST_NAME;
+DESC EMPLOYEE;
+
+-- 9/ 修改EMPLOYEE資料表，新增一個欄位SALARY 資料型態為INT。
+
+ALTER TABLE EMPLOYEE
+ADD COLUMN SALARY INT;
+DESC EMPLOYEE;
+
+-- 10/ 修改EMPLOYEE資料表，使用ID欄位新增一個Primary key的限制條件(constraint)，並為他命名。
+
+ALTER TABLE EMPLOYEE
+ADD CONSTRAINT id PRIMARY KEY(ID);
+DESC EMPLOYEE;
+
+-- 11/ 在EMPLOYEE資料表新增一個外來鍵(foreign key)以確保員工不會被分派到一個不存在的部門。
+
+ALTER TABLE EMPLOYEE
+ADD CONSTRAINT FK_dept_id FOREIGN KEY(dept_id) REFERENCES dept (deptno);
+SHOW INDEXES FROM employee;
